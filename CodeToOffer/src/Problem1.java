32df3ae97254818f2fb6f304848fa58d98e484f2
manojkumarsmks <manojkumarsmks@gmail.com>
2024-01-19 09:26:43 -0700

    Optimized for time to O(n)

--- CodeToOffer/src/Problem1.java
+++ CodeToOffer/src/Problem1.java
@@ -1,13 +1,26 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Problem1 {
 
     public static void main(String[] args) {
-        int[] nums = { 2, 7, 11, 15 };
+        int[] nums = { 3, 3 };
 
-        System.out.println(Arrays.toString(twoSum(nums, 9)));
+        System.out.println(Arrays.toString(twoSumSol(nums, 9)));
     }
 
+    /**
+     * The function "twoSum" takes an array of integers and a target integer as
+     * input, and returns an array of two indices
+     * where the corresponding elements add up to the target.
+     * 
+     * Basic O(n^2) solution.
+     * 
+     * @param nums   An array of integers.
+     * @param target The target is the sum that we are trying to find in the array.
+     * @return The method is returning an array of integers.
+     */
     public static int[] twoSum(int[] nums, int target) {
 
         int[] res = new int[2];
@@ -22,6 +35,47 @@ public class Problem1 {
         }
 
         return res;
+
+    }
+
+    /**
+     * The function "twoSumSol" takes an array of integers and a target integer as
+     * input, and returns an array of two indices
+     * where the corresponding elements add up to the target.
+     * 
+     * The solution uses Hashmap to store the nums position.
+     * 
+     * Time complexity - O(n)
+     * Space complexity - O(n)
+     * 
+     * But we iterate through the map twice.
+     * 
+     * @param nums   An array of integers.
+     * @param target The target is the sum that we are trying to find a pair of
+     *               numbers for.
+     * @return The method `twoSumSol` returns an array of integers.
+     */
+    public static int[] optimizedTwoSum(int[] nums, int target) {
+
+        int[] res = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff) && map.get(diff) != i) {
+                res[0] = i;
+                res[1] = map.get(diff);
+                return res;
+            }
+        }
+
+        return null;
 
     }
 
