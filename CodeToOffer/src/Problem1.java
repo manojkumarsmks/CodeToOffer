2607c3ebb903e6470b5bc2ae40e94a84dc003483
manojkumarsmks <manojkumarsmks@gmail.com>
2024-01-18 20:41:27 -0700

    Basic O(n^2) solution

---
+++ CodeToOffer/src/Problem1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+
+        return res;
+
+    }
+
+}
